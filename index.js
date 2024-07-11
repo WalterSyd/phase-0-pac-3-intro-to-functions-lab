fca7355cd73132f34b3975fb452e1a9287495725
Waltersyd <sydney.onyango@student.moringaschool.com>
2024-07-11 14:14:24 +0300

    Index.js

---
+++ index.js
@@ -0,0 +1,35 @@
+//Function for Return Shout
+function shout(string){
+    return string.toUpperCase();
+};
+//Function for LogShout
+function logShout(Hello){
+    console.log(Hello.toUpperCase());
+}
+
+
+//Function for Whisper
+function whisper(string){
+    return string.toLowerCase();
+};
+//Function for logWhisper
+function logWhisper(Hello){
+    console.log(Hello.toLowerCase());
+};
+
+
+//FUNCTION FOR SAY HI
+
+let toUpperCase = string.toUpperCase
+let toLowerCase = string.toLowerCase
+function sayHiToHeadphonedRoommate(string){
+    if(string === string.toLowerCase()){
+        return "I can't hear you!";
+    }else if(string === string.toUpperCase()){
+        return "YES INDEED!";
+    }else if(string === "Let's have dinner together!"){
+        return "I would love to!";
+    }
+};
+
+    
